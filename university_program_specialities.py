5f87b620807f2f54922c7a0bb78b4d27a5256597
hubble3908 <51421921+hubble3908@users.noreply.github.com>
2019-07-02 13:24:40 -0400

    Dictionary for university specialities
    I am having some trouble saving the university program values into the dictionary. It saves each value as either 'k\n' or'\n'

--- university_program_specialities.py
+++ university_program_specialities.py
@@ -11,13 +11,35 @@ import sys
 import os
 import csv
 
+p_speciality = {}
+
+#Openning AAG csv that contains all of the program specialities
 program_specialities_csv_name = r'program_specialities.csv'
 program_specialities_csv = open(program_specialities_csv_name).readlines()
+
 headers = program_specialities_csv[0].split('\t')
 program_specialities1 = program_specialities_csv[1].split(',')
 program_specialities2 = program_specialities_csv[2].split(',')
-subject_specialities = program_specialities1[9:15] + program_specialities2[0:30]
+del program_specialities2[18]
+subject_specialities = program_specialities1[1:15] + program_specialities2[0:]
 #stopped here have a list of my specialities now
+university_specialties= []
+u_s=[]
+#k = program_specialities_csv[5:].split(',')
 
+for uni in program_specialities_csv[5:]:
+    university_info = uni.split(',')
+    #print(uni)
+    university_name = university_info[0]
+    university = uni.split(',')
+    #print(university)
+    if university not in university_specialties:
+        university_specialties.append(university)
 
-
+for university_details in university_specialties:
+    p_speciality[university_details[0]]= {}
+    for speciality in subject_specialities:
+        for val in university_details:
+            if val not in p_speciality:
+                try: p_speciality[university_details[0]][speciality]= {val}
+                except: 'no value'
