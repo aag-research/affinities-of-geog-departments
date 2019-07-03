9ddb6d74daa6e2595d07aab67885d20f947204a7
hubble3908 <51421921+hubble3908@users.noreply.github.com>
2019-07-03 12:45:38 -0400

    I am just testing this there were no edits made

--- university_program_specialities.py
+++ university_program_specialities.py
@@ -1,4 +1,4 @@
-# Author:                       Eni Awowale
+# Author:                       Eni Awowale & Coline Dony
 # Date first written:           June 27, 2019
 # Date last updated:            July 2, 2019
 # Purpose:                      Determine U.S. University's levels of specialization
@@ -9,43 +9,9 @@
 
 # Set up environment
 import os
-folder = r'C:\Users\cdony\Google Drive\GitHub\affinities-of-geog-departments'
+folder = r'C:\Users\oawowale\Documents\GitHub\affinities-of-geog-departments'
 os.chdir(folder)
 
-<<<<<<< HEAD
-p_speciality = {}
-
-#Openning AAG csv that contains all of the program specialities
-program_specialities_csv_name = r'program_specialities.csv'
-program_specialities_csv = open(program_specialities_csv_name).readlines()
-
-headers = program_specialities_csv[0].split('\t')
-program_specialities1 = program_specialities_csv[1].split(',')
-program_specialities2 = program_specialities_csv[2].split(',')
-del program_specialities2[18]
-subject_specialities = program_specialities1[1:15] + program_specialities2[0:]
-#stopped here have a list of my specialities now
-university_specialties= []
-u_s=[]
-#k = program_specialities_csv[5:].split(',')
-
-for uni in program_specialities_csv[5:]:
-    university_info = uni.split(',')
-    #print(uni)
-    university_name = university_info[0]
-    university = uni.split(',')
-    #print(university)
-    if university not in university_specialties:
-        university_specialties.append(university)
-
-for university_details in university_specialties:
-    p_speciality[university_details[0]]= {}
-    for speciality in subject_specialities:
-        for val in university_details:
-            if val not in p_speciality:
-                try: p_speciality[university_details[0]][speciality]= {val}
-                except: 'no value'
-=======
 # Read input data
 input_filename = r'program_specialities.txt'
 input_as_text = open(input_filename).readlines()
@@ -59,6 +25,7 @@ headers[0] = 'University name'
 geog_programs_data_db = {}
 for line_as_text in input_as_text[3:]:
        line_as_list = line_as_text.split('\t')
+       #print(line_as_list)
        if line_as_list[1] != '':
               program_data = dict(zip(headers, line_as_list))
               geog_programs_data_db[program_data['University name']] = program_data
@@ -69,8 +36,41 @@ specialties = headers[9:-12]
 data_for_radar_chart = [specialties]
 for university, program_data in geog_programs_data_db.items():
        program_specialties = [1 if program_data[specialty] == 'X' else 0 for specialty in specialties]
+       #print(program_specialties)
        data_for_radar_chart += [(university, program_specialties)]
 
+program_data_values={}
+program_values=[]
+for specialty in specialties:
+       program_data_values[specialty]={}
+       for value in program_specialties:
+              #print(program_specialties[34])
+              program_values.append(value)
+count = 0
+for key, value in geog_programs_data_db.items():
+       #print(key)
+       for content in value.values():
+              #print(content)
+              if content == 'X':
+                     count=count+1
+                     program_data_v = dict(specialties)
+
+# for key, value in geog_programs_data_db.items():
+#        #print(value['Agricultural Geography'])
+#        if value['Agricultural Geography'] == 'X':
+#               count=count+1
+#               program_data_values['Agricultural Geography'] = {count}
+
+
+
+
+
+
+# for val in data_for_radar_chart[1:]:
+#        program_data_values[val[0]]={}
+#     # for v in val[1]:
+    #     program_data_values={}
+#stopped here finish tomorrow just need to save key values into program_data_values
+
 # Show the formatted data on the screen
-print(data_for_radar_chart[0:8])
->>>>>>> 8298ba66b0bb3fdd053107ab1724d40e31205318
+#print(data_for_radar_chart[0:8])
