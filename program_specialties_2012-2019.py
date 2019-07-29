b57a50b1fa2234b308c00dec70aad31d65817040
hubble3908 <51421921+hubble3908@users.noreply.github.com>
2019-07-29 16:43:30 -0400

    Created specialty groups and added specific specialties to specialty groups
    still need to reformat all university names and create speciality groups for 2014-2019 (not including 2016)

--- program_specialties_2012-2019.py
+++ program_specialties_2012-2019.py
@@ -19,7 +19,7 @@ os.chdir(folder)
 #Excluding 2016 temporarily because it is in different format than the rest
 unwanted_specialties = ['Program Specialties', 'Associates', 'Bachelors','Masters','PhD', 'Certificate Program','Distance / Online','North America', 'Middle America', 'South America', 'Europe', 'Africa', 'Asia', 'Australia Oceania',
                              'Polar World', 'Middle East', 'Former Soviet Union', 'World Regional\n', '\t', '\n','']
-
+specialty_groups_db={}
 geog_programs_data_db = {}
 for year in [2012, 2014, 2015, 2016, 2017, 2018, 2019]:
     # Need to create exception for 2012 due to formatting
@@ -36,6 +36,15 @@ for year in [2012, 2014, 2015, 2016, 2017, 2018, 2019]:
                 if university_name_2012 not in geog_programs_data_db:
                     geog_programs_data_db[university_name_2012] = {}
                 geog_programs_data_db[university_name_2012][year] = program_data_2012
+                geog_programs_data_db[university_name_2012][year]['Specialty groups'] = {}
+                geog_programs_data_db[university_name_2012][year]['Specialty groups'] = {'Human Geography': [headers_2012[specialty_2012] for specialty_2012 in [16,20,27,32,39,38,33,29]],
+                                                                                         'Human-Environmental Interactions': [headers_2012[specialty_2012] for specialty_2012 in [15,14,19,9,42,26,]],
+                                                                                         'Physical Geography': [headers_2012[specialty_2012] for specialty_2012 in [11,13,23,30]],
+                                                                                         'Geospatial Technologies': [headers_2012[specialty_2012] for specialty_2012 in [37,24,25]],
+                                                                                         'Urban and Economic Geography': [headers_2012[specialty_2012] for specialty_2012 in [41,36,35,31,40,17,18]],
+                                                                                         'Methods': [headers_2012[specialty_2012] for specialty_2012 in [21,22,28,10,12,34,]]}
+                #geog_programs_data_db[university_name_2012][year]['Specialty groups'] = headers_2012[headers_2012.index(
+                    #'Agricultural Geography'): headers_2012.index('Regional Geography')]
     else:
         input_filename = r'program_specialties_%s.txt' % year
         input_as_text = open(input_filename).readlines()
@@ -43,13 +52,37 @@ for year in [2012, 2014, 2015, 2016, 2017, 2018, 2019]:
         else: headers_index = 0
         headers = input_as_text[headers_index].split('\t')
         headers[0] = 'University name'
-    #print(input_as_text[0].split('\t')[15])
-    #print(input_as_text[0:2])
         for line_as_text in input_as_text[headers_index+1:]:
             line_as_list = line_as_text.split('\t')
             if 'X' in line_as_list:
                 program_data = dict(zip(headers, line_as_list))
                 university_name = program_data['University name']
+        #start modifying the university names here
                 if university_name not in geog_programs_data_db:
                     geog_programs_data_db[university_name] = {}
                 geog_programs_data_db[university_name][year] = program_data
+                geog_programs_data_db[university_name][year]['Specialty groups'] = {}
+                if year == 2016:
+                    headers_2016 = headers
+                    geog_programs_data_db[university_name][year]['Specialty groups'] = {
+                    'Human Geography': [headers_2016[specialty_2016] for specialty_2016 in
+                                        [8, 14, 21, 26, 32, 27, 33, 23]],
+                    'Human-Environmental Interactions': [headers_2016[specialty_2016] for specialty_2016 in
+                                                         [13, 7, 6, 1, 36, 20, 12]],
+                    'Physical Geography': [headers_2016[specialty_2016] for specialty_2016 in [3, 5, 17, 24]],
+                    'Geospatial Technologies': [headers_2016[specialty_2012] for specialty_2012 in [18, 19, 31]],
+                    'Urban and Economic Geography': [headers_2016[specialty_2016] for specialty_2016 in
+                                                     [35, 30, 29, 25, 34, 10, 11]],
+                    'Methods': [headers_2016[specialty_2016] for specialty_2016 in [15, 16, 22, 2, 4, 28]]}
+
+
+
+
+
+
+
+
+
+
+#geog_programs_data_db[university_name][year]['Specialty groups'] = headers[headers.index(
+#'Agricultural Geography'): headers.index('North America')]
