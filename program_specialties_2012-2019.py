4bd2e96acbb40cde83efd9f5a7d684abb3df0853
hubble3908 <51421921+hubble3908@users.noreply.github.com>
2019-07-30 16:28:21 -0400

    Update to program specialties
    I made comments on the parts I needed some help on

--- program_specialties_2012-2019.py
+++ program_specialties_2012-2019.py
@@ -9,6 +9,7 @@
 
 # Set up environment
 import os
+import sys
 
 folder = r'C:\Users\oawowale\Documents\GitHub\affinities-of-geog-departments\program_specialties'
 #folder = r'C:\Users\cdony\Google Drive\GitHub\affinities-of-geog-departments'
@@ -28,13 +29,18 @@ for year in [2012, 2014, 2015, 2016, 2017, 2018, 2019]:
         input_as_text_2012 = open(file_name_2012).readlines()
         headers_2012 = ''.join(input_as_text_2012[1:3]).replace('\n', '').split('\t')
         headers_2012[0] = 'University name'
-        for line_as_text_2012 in input_as_text_2012[3:]:
+        for line_as_text_2012 in input_as_text_2012[3:272]:
             line_as_list_2012 = line_as_text_2012.split('\t')
             if 'X' in line_as_list_2012:
                 program_data_2012 = dict(zip(headers_2012, line_as_list_2012))
-                university_name_2012 = program_data_2012['University name']
-                if university_name_2012 not in geog_programs_data_db:
-                    geog_programs_data_db[university_name_2012] = {}
+                university_name_2012_draft = program_data_2012['University name']
+                university_name_2012 = university_name_2012_draft.replace(', ', ' ').replace(' - ', ' ').replace('-', ' ')\
+                    .replace('"','').replace('University of Alaska', 'University of Alaska Fairbanks').replace('.', '')\
+                    .replace('United States', 'US').replace(' at ', ' ').replace('University of Hawaii', 'University of Hawaii Manoa')\
+                    .replace('Univeristy', 'University').replace(' of the State University of New York', ' SUNY').replace(' State University of New York', ' SUNY')\
+                    .replace(' of the City University of New York', ' CUNY').replace('State University of New York', 'SUNY').replace('University of Arkansas Fayettville', 'University of Arkansas')
+                university_name_2012 = university_name_2012.lower()
+                geog_programs_data_db[university_name_2012] = {}
                 geog_programs_data_db[university_name_2012][year] = program_data_2012
                 geog_programs_data_db[university_name_2012][year]['Specialty groups'] = {}
                 geog_programs_data_db[university_name_2012][year]['Specialty groups'] = {'Human Geography': [headers_2012[specialty_2012] for specialty_2012 in [16,20,27,32,39,38,33,29]],
@@ -43,8 +49,6 @@ for year in [2012, 2014, 2015, 2016, 2017, 2018, 2019]:
                                                                                          'Geospatial Technologies': [headers_2012[specialty_2012] for specialty_2012 in [37,24,25]],
                                                                                          'Urban and Economic Geography': [headers_2012[specialty_2012] for specialty_2012 in [41,36,35,31,40,17,18]],
                                                                                          'Methods': [headers_2012[specialty_2012] for specialty_2012 in [21,22,28,10,12,34,]]}
-                #geog_programs_data_db[university_name_2012][year]['Specialty groups'] = headers_2012[headers_2012.index(
-                    #'Agricultural Geography'): headers_2012.index('Regional Geography')]
     else:
         input_filename = r'program_specialties_%s.txt' % year
         input_as_text = open(input_filename).readlines()
@@ -52,16 +56,20 @@ for year in [2012, 2014, 2015, 2016, 2017, 2018, 2019]:
         else: headers_index = 0
         headers = input_as_text[headers_index].split('\t')
         headers[0] = 'University name'
+        #do you think you can help format this so it stops before it gets to Canada
         for line_as_text in input_as_text[headers_index+1:]:
             line_as_list = line_as_text.split('\t')
             if 'X' in line_as_list:
                 program_data = dict(zip(headers, line_as_list))
-                university_name = program_data['University name']
-        #start modifying the university names here
+                university_name_draft = program_data['University name']
+                #start modifying the university names here
+                university_name = university_name_draft.replace(', ', ' ').replace(' - ', ' ').replace('-', ' ').replace('"', '') \
+                    .replace('.', '').replace('United States', 'US').replace(' at ', ' ').replace('Univeristy', 'University').replace(' (UCLA)', '') \
+                    .replace('University of Arkansas Fayettville', 'University of Arkansas').replace('University of Arkansas Fayetteville', 'University of Arkansas')
+                university_name = university_name.lower()
                 if university_name not in geog_programs_data_db:
                     geog_programs_data_db[university_name] = {}
                 geog_programs_data_db[university_name][year] = program_data
-                geog_programs_data_db[university_name][year]['Specialty groups'] = {}
                 if year == 2016:
                     headers_2016 = headers
                     geog_programs_data_db[university_name][year]['Specialty groups'] = {
@@ -74,15 +82,14 @@ for year in [2012, 2014, 2015, 2016, 2017, 2018, 2019]:
                     'Urban and Economic Geography': [headers_2016[specialty_2016] for specialty_2016 in
                                                      [35, 30, 29, 25, 34, 10, 11]],
                     'Methods': [headers_2016[specialty_2016] for specialty_2016 in [15, 16, 22, 2, 4, 28]]}
+                geog_programs_data_db[university_name][year]['Specialty groups'] = {}
+    #trying to figure out a method for organizing the original specialties into the group specialties for the years
+    #2014, 2015, 2017, 2018, and 2019. 2014 and 2015 have the same number of specialties/headers while 2018 and 2019
+    #2016 has the same number 2016 but i did 2016 separately because it has 'Distance / Online Program' as a unique speciality
 
+#use to check for repeated universities
+for university, years in geog_programs_data_db.items():
+    if len(years) < 4:
+        #print(university, len(years))
+        print(len(years),university, years)
 
-
-
-
-
-
-
-
-
-#geog_programs_data_db[university_name][year]['Specialty groups'] = headers[headers.index(
-#'Agricultural Geography'): headers.index('North America')]
